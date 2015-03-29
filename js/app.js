79ebc398a0370266ae3b51d289f03895301511ec
Joe Haines <hello@joehaines.co.uk>
2015-03-29 14:45:57 +0100

    Cleaning up app.js

--- js/app.js
+++ js/app.js
@@ -9,33 +9,41 @@ var app = {
             null,
             function(response) {
                 try {
-                    var listIndex = $l.helpers.random(0, response.possibleLists.length - 1);
-                    var listType = response.possibleLists[listIndex];
-                    var list = response[listType];
-                    var messages = list.messages;
-
-                    var messageOutputter = new MessageOutputter(
-                        new MessageGenerator(),
-                        new MessageList()
-                            .setList(list.messages, list.prefix, list.suffix)
-                            .getList()
-                    );
-
-                    messageOutputter.doOutput('message-output');
+                    app.outputFromJsonList(response);
                 } catch (e) {
-                    app.outputPredefinedList();
+                    app.outputFromPredefinedList();
                 }
             },
             function(response) {
-                app.outputPredefinedList();
+                app.outputFromPredefinedList();
             }
         );
     },
 
     /**
-     * Outputs a predefined list in cases where the ajax call fails
+     * Outputs a message from a JSON object
+     * @param  {object} response An object returned from ajax call
      */
-    outputPredefinedList: function() {
+    outputFromJsonList: function(response) {
+        var listIndex = $l.helpers.random(0, response.possibleLists.length - 1);
+        var listType = response.possibleLists[listIndex];
+        var list = response[listType];
+        var messages = list.messages;
+
+        var messageOutputter = new MessageOutputter(
+            new MessageGenerator(),
+            new MessageList()
+                .setList(list.messages, list.prefix, list.suffix)
+                .getList()
+        );
+
+        messageOutputter.doOutput('message-output');
+    },
+
+    /**
+     * Outputs a message from predefined list in cases where the ajax call fails
+     */
+    outputFromPredefinedList: function() {
         var messageOutputter = new MessageOutputter(
             new MessageGenerator(),
             new MessageList().setPredefinedList().getList()
